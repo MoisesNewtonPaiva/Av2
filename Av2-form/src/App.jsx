a3d64931be284ebf4f8a238e8a2894757d743991
MoisesNewtonPaiva <145510766+MoisesNewtonPaiva@users.noreply.github.com>
2025-06-23 22:20:42 -0300

    Primeiro

---
+++ Av2-form/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/formulario" element={<Formulario />} />
+    </Routes>
+  );
+}
